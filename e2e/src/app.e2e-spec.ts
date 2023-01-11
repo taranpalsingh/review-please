0e7d7ade2050235ad6d5f12137576223d80f35d8
taranpalsingh <taranpal.singh23@gmail.com>
2023-01-10 23:55:35 -0500

    name changed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('fis-simulator app is running!');
+    expect(await page.getTitleText()).toEqual('review-please app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fis-simulator'`, () => {
+  it(`should have as title 'review-please'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fis-simulator');
+    expect(app.title).toEqual('review-please');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fis-simulator app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('review-please app is running!');
   });
 });
